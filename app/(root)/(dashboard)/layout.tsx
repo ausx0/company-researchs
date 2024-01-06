cb3c53dc6d946ea450b2403535b34e373ddcdfd2
ausx0 <ahmedalnaemy112233@gmail.com>
2024-01-06 12:30:28 +0300

    add: the layout for dashboard page

--- app/(root)/(dashboard)/layout.tsx
+++ app/(root)/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import SideBar from "./components/SideBar";
+import NavBar from "./components/NavBar";
 
 export default function DashboardLayout({
   children, // will be a page or nested layout
@@ -6,10 +7,18 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="h-screen flex flex-row justify-start">
+    <section className="h-screen flex flex-row justify-start overflow-hidden">
       {/* Include shared UI here e.g. a header or sidebar */}
-      <SideBar />
-      <div className="bg-primary flex-1">{children}</div>
+      <section className="overflow-auto  scrollbar-thumb-gray-200 scrollbar-none hover:scrollbar-thumb-gray-300 ">
+        <SideBar />
+      </section>
+      <div className="flex flex-col  flex-1">
+        <NavBar />
+
+        <div className=" flex-1 p-4 overflow-auto scrollbar-none">
+          {children}
+        </div>
+      </div>
     </section>
   );
 }
